1805055e7655b4f21a6ee92aee053016613cc0b5
valchevgd <valchevgd@gmail.com>
2020-05-16 14:48:34 +0300

    Create Specialty Map Service Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/com/github/valchevgd/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.github.valchevgd.sfgpetclinic.services;
+
+import com.github.valchevgd.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/github/valchevgd/sfgpetclinic/services/map/SpecialtyServiceMap.java
@@ -0,0 +1,35 @@
+package com.github.valchevgd.sfgpetclinic.services.map;
+
+import com.github.valchevgd.sfgpetclinic.model.Speciality;
+import com.github.valchevgd.sfgpetclinic.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyServiceMap extends AbstractMapService<Speciality, Long> implements SpecialtiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        super.delete(speciality);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return super.save(speciality);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
