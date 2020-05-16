a07d0e8b24aec84aaa7f1eeab592a2e42af21cfa
valchevgd <valchevgd@gmail.com>
2020-05-16 13:25:12 +0300

    Create PetType Map Service Impl. Closes #29

---
+++ pet-clinic-data/src/main/java/com/github/valchevgd/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.github.valchevgd.sfgpetclinic.services;
+
+import com.github.valchevgd.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
