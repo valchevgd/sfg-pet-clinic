5d96eb01ef09d397d93191058a8c2c1402a14949
valchevgd <valchevgd@gmail.com>
2020-05-18 22:43:22 +0300

    Create Spring Data JPA Service for Visit. Closes #47

---
+++ pet-clinic-data/src/main/java/com/github/valchevgd/sfgpetclinic/services/springdatajpa/VisitSDJpaService.java
@@ -0,0 +1,48 @@
+package com.github.valchevgd.sfgpetclinic.services.springdatajpa;
+
+import com.github.valchevgd.sfgpetclinic.model.Visit;
+import com.github.valchevgd.sfgpetclinic.repositories.VisitRepository;
+import com.github.valchevgd.sfgpetclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VisitSDJpaService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitSDJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return visitRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        return visitRepository.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        visitRepository.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        visitRepository.deleteById(id);
+    }
+}
