c022bb9712a90989c9873b35dae6592b63f54e41
valchevgd <valchevgd@gmail.com>
2020-05-19 08:19:10 +0300

    AllArgs and NoArgs Constructors in Base Entity and Person. Closes #51

--- pet-clinic-data/src/main/java/com/github/valchevgd/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/github/valchevgd/sfgpetclinic/model/Person.java
@@ -1,6 +1,8 @@
 package com.github.valchevgd.sfgpetclinic.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Column;
@@ -8,6 +10,8 @@ import javax.persistence.MappedSuperclass;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @MappedSuperclass
 public class Person extends BaseEntity {
 
